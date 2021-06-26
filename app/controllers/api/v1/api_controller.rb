b9b2bad3812e9383bd27fef3e8c70ae0169d541f
Yukishige Nakajo <nakajo@chaintope.com>
2021-06-26 14:52:20 +0900

    buf fix. Fix 'Not defined current_resource_owner' error.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApiController < ApplicationController
-  private
+  protected
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
